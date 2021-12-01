458e942b90e6c1582f14e56a176359bea9800352
iyaaelah <mrifqiramadhan@gmail.com>
2021-12-01 14:44:24 +0700

    pertemuan minggu ini

--- resources/views/pages/header.blade.php
+++ resources/views/pages/header.blade.php
@@ -1,1 +1,14 @@
-<h1 class="text-danger">Ini adalah Section Header</h1>
+<header class="masthead">
+    <div class="container">
+      <div class="row align-items-center row-title">
+        <div class="col-sm-3 text-center" data-aos="fade-right" 
+        data-aos-duration="1500">
+            <img src="{{ asset('img/gambarprofil.JPG') }}" class="img-fluid my-photo" alt="my-photo">
+        </div>
+        <div class="col-sm-9 text-center" data-aos="fade-up" data-aos-duration="1500">
+            <div class="my-name">Hi, I'm <strong>Rifqi</strong></div>
+            <div class="caption">Mahasiswa TI Gunadarma</div>
+        </div>
+      </div>
+    </div>
+  </header>
